b45b165e863503fb875d92f9d8f5d8d96a61246e
EmmaGau <emma.gauillard@student-cs.fr>
2023-12-06 15:18:27 +0100

    test change

--- test.py
+++ test.py
@@ -8,14 +8,14 @@ model = Unet(
 
 diffusion = GaussianDiffusion(
     model,
-    image_size = 128,
+    image_size = 256,
     timesteps = 1000,           # number of steps
     sampling_timesteps = 250    # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
 )
 
 trainer = Trainer(
     diffusion,
-    'path/to/your/images',
+    'data/Bitewings_Resized_256',
     train_batch_size = 32,
     train_lr = 8e-5,
     train_num_steps = 700000,         # total training steps
