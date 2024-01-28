9884f8d8d37be228007a6be15e072bcd4136e206
Quentin Gopée <quentin.gopee@student-cs.fr>
2024-01-28 15:38:11 +0100

    compute miou

--- miou.py
+++ miou.py
@@ -4,35 +4,48 @@ import os
 import cv2
 import argparse
 
-class_colors = [[255, 0, 0], [0, 255, 0], [0, 0, 255], [255, 152, 0], [255, 152, 0], [0, 255, 237]]
+categories = {
+    'Email': ([255, 0, 0], 1),
+    'Os': ([0, 255, 0], 2),
+    'Dentine': ([0, 0, 255], 3),
+    'Autre': ([255, 152, 0], 4),
+    'Carie': ([255, 152, 0], 5),
+    'Pulpe': ([0, 255, 237], 6)
+}
 
-def calculate_iou(gt_mask, pred_mask, num_classes):
-    iou_sum = 0.0
-    for class_color in class_colors:  # Class IDs start from 1
-        gt_class_mask = (gt_mask == class_color).astype(np.uint8)
-        pred_class_mask = (pred_mask == class_color).astype(np.uint8)
-        # resize the mask to 64xx64
-        gt_class_mask = cv2.resize(gt_class_mask, (64, 64))
+def calculate_iou(gt_mask, pred_mask):
+    intersection = np.logical_and(gt_mask, pred_mask)
+    union = np.logical_or(gt_mask, pred_mask)
+    iou = np.sum(intersection) / (np.sum(union) + 1e-10)  # Add a small epsilon to avoid division by zero
+    return iou
 
-        intersection = np.logical_and(gt_class_mask, pred_class_mask)
-        union = np.logical_or(gt_class_mask, pred_class_mask)
-        
-        iou = np.sum(intersection) / (np.sum(union) + 1e-10)  # Add a small epsilon to avoid division by zero
-        iou_sum += iou
-    return iou_sum / num_classes
-
-def calculate_miou(gt_masks, pred_masks, num_classes):
+def calculate_miou(gt_masks, pred_masks, categories):
+    num_classes = len(categories)
     total_iou = 0.0
+    class_iou = np.zeros(num_classes)
     num_samples = len(gt_masks)
 
     for i in range(num_samples):
         gt_mask = np.array(Image.open(gt_masks[i]))
         pred_mask = np.array(Image.open(pred_masks[i]))
 
-        iou = calculate_iou(gt_mask, pred_mask, num_classes)
-        total_iou += iou
+        sample_iou = 0.0
+        for class_name, (class_color, class_id) in categories.items():
+            gt_class_mask = (gt_mask == class_color).astype(np.uint8)
+            pred_class_mask = (pred_mask == class_color).astype(np.uint8)
+            gt_class_mask = cv2.resize(gt_class_mask.astype(np.uint8), (64, 64))
+            pred_class_mask = cv2.resize(pred_class_mask.astype(np.uint8), (64, 64))
 
-    return total_iou / num_samples
+            iou = calculate_iou(gt_class_mask, pred_class_mask)
+            sample_iou += iou
+            class_iou[class_id - 1] += iou
+
+        total_iou += sample_iou / num_classes
+
+    mIoU = total_iou / num_samples
+    class_iou /= num_samples
+
+    return mIoU, class_iou
 
 parser = argparse.ArgumentParser()
 parser.add_argument('gt_folder')
@@ -41,7 +54,9 @@ parser.add_argument('results_folder')
 args = parser.parse_args()
 gt_mask_paths = [os.path.join(args.gt_folder, file_name) for file_name in os.listdir(args.gt_folder)]
 pred_mask_paths = [os.path.join(args.results_folder, file_name) for file_name in os.listdir(args.results_folder)]
-num_classes = 6
 
-miou = calculate_miou(gt_mask_paths, pred_mask_paths, num_classes)
+miou, class_iou = calculate_miou(gt_mask_paths, pred_mask_paths, categories)
 print(f"mIoU: {miou}")
+print("mIoU per class:")
+for class_name, (_, class_id) in categories.items():
+    print(f"{class_name}: {class_iou[class_id - 1]}")
