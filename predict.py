bea6a3fd4cbeffb51c13453ff14decc6ad6d00eb
louisreumaux <louis.reumaux@student-cs.fr>
2024-01-31 13:01:08 +0100

    m

--- predict.py
+++ predict.py
@@ -126,113 +126,116 @@ def create_argparser():
     return parser
 
 
-parser = create_argparser()
-parser.add_argument('model_path')
-parser.add_argument('img_dir')
-parser.add_argument('result_dir')
-args = parser.parse_args()
-for k, v in args.__dict__.items():
-    print(k, v)
+def main():
+    parser = create_argparser()
+    parser.add_argument('model_path')
+    parser.add_argument('img_dir')
+    parser.add_argument('result_dir')
+    args = parser.parse_args()
+    for k, v in args.__dict__.items():
+        print(k, v)
 
-dist_util.setup_dist()
-logger.configure()
-model = create_model(
-    args.image_size,
-    args.num_channels,
-    args.num_res_blocks,
-    args.channel_mult,
-    False,
-    args.class_cond,
-    args.use_checkpoint,
-    args.attention_resolutions,
-    args.num_heads,
-    args.num_head_channels,
-    args.num_heads_upsample,
-    args.use_scale_shift_norm,
-    args.dropout,
-    args.resblock_updown,
-    args.use_fp16,
-    args.use_new_attention_order,
-    args.out_channels
-)
+    dist_util.setup_dist()
+    logger.configure()
+    model = create_model(
+        args.image_size,
+        args.num_channels,
+        args.num_res_blocks,
+        args.channel_mult,
+        False,
+        args.class_cond,
+        args.use_checkpoint,
+        args.attention_resolutions,
+        args.num_heads,
+        args.num_head_channels,
+        args.num_heads_upsample,
+        args.use_scale_shift_norm,
+        args.dropout,
+        args.resblock_updown,
+        args.use_fp16,
+        args.use_new_attention_order,
+        args.out_channels
+    )
 
-categories = {
-    'Email': ([255, 0, 0], 1),
-    'Os': ([0, 255, 0], 2),
-    'Dentine': ([0, 0, 255], 3),
-    'Autre': ([255, 0, 254], 4),
-    'Carie': ([255, 152, 0], 5),
-    'Pulpe': ([0, 255, 237], 6)
-}
+    categories = {
+        'Email': ([255, 0, 0], 1),
+        'Os': ([0, 255, 0], 2),
+        'Dentine': ([0, 0, 255], 3),
+        'Autre': ([255, 0, 254], 4),
+        'Carie': ([255, 152, 0], 5),
+        'Pulpe': ([0, 255, 237], 6)
+    }
 
-colors = {v[1]: v[0] for k, v in categories.items()}
+    colors = {v[1]: v[0] for k, v in categories.items()}
 
-device = ('cuda' if torch.cuda.is_available() else 'cpu')
+    device = ('cuda' if torch.cuda.is_available() else 'cpu')
 
-state_dict = torch.load(args.model_path, map_location=device)
-model.load_state_dict(state_dict)
+    state_dict = torch.load(args.model_path, map_location=device)
+    model.load_state_dict(state_dict)
 
-model = model.to(device)
+    model = model.to(device)
 
-for name, param in model.named_parameters():
-    param.requires_grad = False
+    for name, param in model.named_parameters():
+        param.requires_grad = False
+
+    def overlay_images(image, overlay, ignore_color=[0, 0, 0], alpha=0.5):
+        ignore_color = np.asarray(ignore_color)
+        mask = (overlay == ignore_color).all(-1, keepdims=True)
+        out = np.where(mask, image, image * alpha + overlay *
+                       (1 - alpha)).astype(image.dtype)
+        return out
+
+    # Create output directory if needed
+    if not os.path.exists(args.result_dir):
+        os.makedirs(args.result_dir)
+
+    out_dir = ['IMG/', 'MASK/', 'OVERLAY/']
+    for i in out_dir:
+        print(os.path.join(args.result_dir, i))
+        if not os.path.exists(os.path.join(args.result_dir, i)):
+            os.makedirs(os.path.join(args.result_dir, i))
+
+    model.eval()
+    torch.backends.cudnn.deterministic = True
+    for i in os.listdir(args.img_dir):
+        img = cv2.imread(os.path.join(args.img_dir, i))
+        height, width = img.shape[:2]
+
+        img = cv2.resize(img, (args.image_size, args.image_size))
+        orig_img = img.copy()
+        img = img.astype(np.float32) / 127.5 - 1.
+        img = np.moveaxis(img, -1, 0)
+        batch = np.array([img])
+        tensor = torch.from_numpy(batch)
+        tensor = tensor.to(device, dtype=torch.float32)
+
+        with torch.no_grad():
+            outputs = model(tensor, timesteps=torch.full(
+                (tensor.size()[0],), 1, device=device, dtype=torch.int32))
+            outputs = torch.softmax(outputs[0], axis=0)
+            tmp = outputs.detach().cpu().numpy()
+            tmp = np.argmax(tmp, axis=0)
+            res = np.zeros_like(img, dtype=np.uint8).transpose(1, 2, 0)
+
+            for c_number, c_color in colors.items():
+                res[tmp == c_number] = c_color
+
+            # fig, axes = plt.subplots(nrows=1, ncols=3)
+            # figManager = plt.get_current_fig_manager()
+            # figManager.window.showMaximized()
+            orig_img_cpy = orig_img.copy()
+            res_cpy = res.copy()
+            # axes[0].imshow(orig_img)
+            # axes[1].imshow(res)
+            # axes[2].imshow(overlay_images(orig_img_cpy, res_cpy,alpha=0.85))
+            # plt.show()
+
+            print('writing results for', i)
+            cv2.imwrite(os.path.join(args.result_dir, 'IMG/', i), orig_img)
+            cv2.imwrite(os.path.join(args.result_dir, 'MASK/', i), res)
+            cv2.imwrite(os.path.join(args.result_dir, 'OVERLAY/', i),
+                        overlay_images(orig_img_cpy, res_cpy, alpha=0.85))
 
 
-def overlay_images(image, overlay, ignore_color=[0, 0, 0], alpha=0.5):
-    ignore_color = np.asarray(ignore_color)
-    mask = (overlay == ignore_color).all(-1, keepdims=True)
-    out = np.where(mask, image, image * alpha + overlay *
-                   (1 - alpha)).astype(image.dtype)
-    return out
-
-
-# Create output directory if needed
-if not os.path.exists(args.result_dir):
-    os.makedirs(args.result_dir)
-
-out_dir = ['IMG/', 'MASK/', 'OVERLAY/']
-for i in out_dir:
-    print(os.path.join(args.result_dir, i))
-    if not os.path.exists(os.path.join(args.result_dir, i)):
-        os.makedirs(os.path.join(args.result_dir, i))
-
-model.eval()
-torch.backends.cudnn.deterministic = True
-for i in os.listdir(args.img_dir):
-    img = cv2.imread(os.path.join(args.img_dir, i))
-    height, width = img.shape[:2]
-
-    img = cv2.resize(img, (args.image_size, args.image_size))
-    orig_img = img.copy()
-    img = img.astype(np.float32) / 127.5 - 1.
-    img = np.moveaxis(img, -1, 0)
-    batch = np.array([img])
-    tensor = torch.from_numpy(batch)
-    tensor = tensor.to(device, dtype=torch.float32)
-
-    with torch.no_grad():
-        outputs = model(tensor, timesteps=torch.full(
-            (tensor.size()[0],), 1, device=device, dtype=torch.int32))
-        outputs = torch.softmax(outputs[0], axis=0)
-        tmp = outputs.detach().cpu().numpy()
-        tmp = np.argmax(tmp, axis=0)
-        res = np.zeros_like(img, dtype=np.uint8).transpose(1, 2, 0)
-
-        for c_number, c_color in colors.items():
-            res[tmp == c_number] = c_color
-
-        # fig, axes = plt.subplots(nrows=1, ncols=3)
-        # figManager = plt.get_current_fig_manager()
-        # figManager.window.showMaximized()
-        orig_img_cpy = orig_img.copy()
-        res_cpy = res.copy()
-        # axes[0].imshow(orig_img)
-        # axes[1].imshow(res)
-        # axes[2].imshow(overlay_images(orig_img_cpy, res_cpy,alpha=0.85))
-        # plt.show()
-
-        print('writing results for', i)
-        cv2.imwrite(os.path.join(args.result_dir, 'IMG/', i), orig_img)
-        cv2.imwrite(os.path.join(args.result_dir, 'MASK/', i), res)
-        cv2.imwrite(os.path.join(args.result_dir, 'OVERLAY/', i),
-                    overlay_images(orig_img_cpy, res_cpy, alpha=0.85))
+if __name__ == '__main__':
+    main()
